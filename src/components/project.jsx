71ca2dbf45853ecb8f96a42716ec2ebdae0b8fe1
Elizabeth Witengier <ewitengier@ufl.edu>
2023-01-22 19:01:48 -0500

    completed

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -1,41 +1,13 @@
 import styles from '../styles/portfolio.module.css'
 
 
-export default function Project() {
-  const projects = [
-    {
-      id: 1,
-      project_name: "Block by Block Website", 
-      date: 2022,
-      info: "This was an individual project created to design a website landing page"
-    },
-    {
-      id: 2,
-      project_name: "100 Days of Democracy",
-      date: 2022,
-      info: "The was an individual social media campaign created to promote 100 Days of Democracy"
-    },
-    {
-      id: 3,
-      project_name: "Florida Kids Eat Well Logo",
-      date: 2021,
-      info: "The was an individual project created to design a logo for a statewide initiative"
-    },
-    {
-      id: 4, 
-      project_name: "Toxic Exhibition Website",
-      date: 2021,
-      info: "The was an individual project created to teach about sustainable farming"
-    }
-  ]
+export default function Project({project, name, date, info}) {
   
   return (
-    <div>
-      <h1>Testing</h1>
-    
-      {projects.map(project => (
-        <projectList key={project.id} project={project.project_name} date={project.date} info={project.info} />
-      ))}
+    <div className={styles.portfolio_container}>
+      <h2 style={{color: "black"}}>{project}</h2>
+      <div className={styles.portfolio_date}>{date}</div>
+      <p style={{color: "black"}}>{info}</p>
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,45 @@
+import Project from '../components/project'
 import styles from '../styles/home.module.css'
-// import Project from './component/project'
 
 export default function Home() {
+  const projects = [
+    {
+      id: 1,
+      project_name: "Block by Block Website", 
+      date: 2022,
+      info: "This was an individual project created to design a website landing page"
+    },
+    {
+      id: 2,
+      project_name: "100 Days of Democracy",
+
+      date: 2022,
+      info: "The was an individual social media campaign created to promote 100 Days of Democracy"
+    },
+    {
+      id: 3,
+      project_name: "Florida Kids Eat Well Logo",
+      date: 2021,
+      info: "The was an individual project created to design a logo for a statewide initiative"
+    },
+    {
+      id: 4, 
+      project_name: "Toxic Exhibition Website",
+      date: 2022,
+      info: "The was an individual project created to teach about sustainable farming"
+    }
+  ]
   return (
     <main>
       <h1>Elizabeth Witengier</h1>
       <h2 style={{ color: "orange"}}>Designer & Content Creator</h2>
+      <br></br>
       <section>
-        
+        {projects.map(project => (
+          <Project key={project.id} project={project.project_name} date={project.date} info={project.info} />
+        ))}
       </section>
-</main>
+    </main>
   )
 }
 
